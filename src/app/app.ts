32266ef8ce006697807048c980106f874d3c91dc
Manon Ménielle <130688604+mamenielle@users.noreply.github.com>
2026-02-03 10:53:19 +0100

    première ébauche de la page d'index

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Accueil } from './accueil/accueil';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Accueil],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('zz2Project');
 }
+
