505197a6824b6e4a248a32862d23326c629940bb
Emeline Dufrenne <emeline.dufrenne@etu.uca.fr>
2026-02-03 12:28:02 +0100

    ajout route et page ordi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './components/accueil/accueil';
+import { Computer } from './components/computer/computer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',              // route racine
+    redirectTo: 'accueil', // redirige vers accueil
+    pathMatch: 'full'      // obligatoire pour éviter une redirection infinie
+  },
+  {
+    path: 'accueil',
+    component: Accueil,
+  },
+  {
+    path: 'computer',
+    component: Computer,
+  },
+];
