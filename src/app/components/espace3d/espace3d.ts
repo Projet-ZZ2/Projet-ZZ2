cfdd7e1a9eee641b2a54d2ba16733ec59b02ceee
rowan <runetreestudio@gmail.com>
2026-02-03 17:32:31 +0100

    espace 3D component

---
+++ src/app/components/espace3d/espace3d.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-espace3d',
+  imports: [],
+  templateUrl: './espace3d.html',
+  styleUrl: './espace3d.css',
+})
+export class Espace3d {
+
+}
