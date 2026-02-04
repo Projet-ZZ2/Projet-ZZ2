1860dc5d5f0ee962cec192a10be7a571aa5db70a
rowan <runetreestudio@gmail.com>
2026-02-04 09:45:04 +0100

    add route jeu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Accueil } from './components/accueil/accueil';
 import { Computer } from './components/computer/computer';
+import { Espace3d } from './components/espace3d/espace3d';
 
 export const routes: Routes = [
   {
@@ -11,4 +12,8 @@ export const routes: Routes = [
     path: 'computer',
     component: Computer,
   },
+  {
+    path: 'jeu',
+    component: Espace3d,
+  }
 ];
