8422197e8703a36b9df57f986356b13b084da931
Louis <louis.kostadinov@etu.uca.fr>
2026-02-06 10:29:13 +0100

    affichage moteur

--- src/app/components/client-game/client-game.ts
+++ src/app/components/client-game/client-game.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Phaser from 'phaser';
+import StartGame from '../../../assets/phaser/main';
+import { EventBus } from '../../../assets/phaser/EventBus';
 
 @Component({
   selector: 'app-client-game',
-  imports: [],
   templateUrl: './client-game.html',
   styleUrl: './client-game.css',
+  standalone: true
 })
-export class ClientGame {
+export class ClientGame implements OnInit {
+  scene: Phaser.Scene;
+  game : Phaser.Game;
+  sceneCallBack: (scene : Phaser.Scene) => void;
 
+  ngOnInit(): void {
+    this.game=StartGame('game-container');
+
+    EventBus.on('current-scene-ready', (scene : Phaser.Scene) =>
+    {
+      this.scene = scene;
+
+      if(this.sceneCallBack){
+        this.sceneCallBack(scene);
+      }
+    }
+    )
+  }
+
+  ngOnDestroy(){
+    if(this.game){
+      this.game.destroy(true);
+    }
+  }
 }
--- src/assets/phaser/main.ts
+++ src/assets/phaser/main.ts
@@ -1,4 +1,5 @@
 import { AUTO, Game } from 'phaser';
+import {Game as MainGame} from './scenes/Game'
 
 // Find out more information about the Game Config at:
 // https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
@@ -8,13 +9,14 @@ const config: Phaser.Types.Core.GameConfig = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
-    scene: [ ]
+    scene: [
+        MainGame
+     ]
 };
 
 const StartGame = (parent: string) => {
 
     return new Game({ ...config, parent });
-
 }
 
 export default StartGame;
